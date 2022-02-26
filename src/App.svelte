4ac775ba95068b5f6c983b3c6737cba598549739
Mathieu Keller <business@mathieu.tech>
2022-02-26 11:29:28 +0000

    add contractcard

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import ContactCard from './ContractCard.svelte';
 	let name = 'Mathieu';
 	let age = 20;
 
@@ -7,11 +8,10 @@
 	}
 </script>
 
-<main>
-	<h1>Hello {name}, my age is {age}</h1>
-	<button on:click="{incremetAge}">Change age</button>
-	<input type="text" bind:value="{name}" />
-</main>
+<h1>Hello {name}, my age is {age}</h1>
+<button on:click="{incremetAge}">Change age</button>
+<input type="text" bind:value="{name}" />
+<ContactCard name={name}/>
 
 <style>
 	h1{
